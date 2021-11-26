eb04fc744fff957f29f31408e9224665f70206c1
taeheonOH <oth5607@gmail.com>
2021-11-26 15:40:28 +0900

    step1

---
+++ 16_lacf.tf
@@ -0,0 +1,14 @@
+resource "aws_launch_configuration" "thoh_lacf" {
+  name = "thoh-ami"
+  image_id = aws_ami_from_instance.thoh_ami.id
+  instance_type = "t2.micro"
+  iam_instance_profile = "admin_role"
+  security_groups = [aws_security_group.thoh_sg.id]
+  key_name = "thoh-key"
+  user_data =<<-EOF
+                #!/bin/bash
+                systemctl start httpd
+                systemctl enable httpd
+                EOF
+
+}
