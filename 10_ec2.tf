b669d9f1eab3000b70b527edd44ac00a8114095e
taeheonOH <oth5607@gmail.com>
2021-11-26 10:34:35 +0900

    step1

---
+++ 10_ec2.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "amzn" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "thoh_web" {
+ ami = data.aws_ami.amzn.id
+ instance_type = "t2.micro"
+ key_name = "thoh-key"
+ vpc_security_group_ids = [aws_security_group.thoh_sg.id]
+ availability_zone = "ap-northeast-2a"
+ private_ip = "10.0.0.11"
+ subnet_id = aws_subnet.thoh_puba.id
+
+ tags = {
+     "Name" = "thoh-web"
+ }
+}
+
+resource "aws_eip" "thoh_web_ip" {
+  vpc = true
+  instance = aws_instance.thoh_web.id
+  associate_with_private_ip = "10.0.0.11"
+  depends_on = [aws_internet_gateway.thoh_ig]  
+}
+
+output "public_ip" {
+  value = aws_instance.thoh_web.public_ip
+}
