0fee9d6d213fbc12476e045cf58a2b5ef9417ee4
taeheonOH <oth5607@gmail.com>
2021-11-29 11:33:15 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -10,7 +10,7 @@ resource "aws_autoscaling_group" "thoh_atsg" {
   health_check_grace_period = 60
   health_check_type = "EC2"
   desired_capacity = 2
-  force_delete = true
+  force_delete = false
   launch_configuration = aws_launch_configuration.thoh_lacf.name
   #placement_group = aws_placement_group.thoh_pg.id
   vpc_zone_identifier = [aws_subnet.thoh_puba.id,aws_subnet.thoh_pubc.id]   
