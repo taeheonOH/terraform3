6edbbe627dd82577ded07acf8ebe5cde4a253340
taeheonOH <oth5607@gmail.com>
2021-11-30 10:07:46 +0900

    step1

--- 01_main.tf
+++ 01_main.tf
@@ -1,47 +1,12 @@
 provider "aws" {
-    region = var.region
-}
-
-resource "aws_key_pair" "thoh_key" {
-  key_name = var.key
-  public_key = file("../../.ssh/thoh-key.pub")
+    region = "ap-northeast-2"
 }
 
 resource "aws_vpc" "thoh_vpc"{
-  cidr_block = var.cidr_main
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = {
-      "Name" = "${var.name}-vpc"
+      "Name" = "thoh-vpc"
   }
-}
-
-resource "aws_subnet"  "thoh_pub" {
-  count = length(var.pub_sub)      #2  
-  vpc_id = aws_vpc.thoh_vpc.id
-  cidr_block = var.pub_sub[count.index] #index 0 -> 10.0.0.0/24
-  availability_zone = "${var.region}${var.ava_zone[count.index]}"
-  tags = {
-    "Name" = "${var.name}-pub${var.ava_zone[count.index]}"  
-  }
-}
-
-resource "aws_subnet" "thoh_pri" {
-  count = 2  
-  vpc_id = aws_vpc.thoh_vpc.id
-  cidr_block = var.pri_sub[count.index]
-  availability_zone = "${var.region}${var.ava_zone[count.index]}"
-  tags = {
-    "Name" = "${var.name}-pri${var.ava_zone[count.index]}"  
-  } 
-}
-
-resource "aws_subnet"  "thoh_pridb" {
-  count = 2  
-  vpc_id = aws_vpc.thoh_vpc.id
-  cidr_block = var.pri_subdb[count.index]
-  availability_zone = "${var.region}${var.ava_zone[count.index]}"
-  tags = {
-    "Name" = "${var.name}-pridb${var.ava_zone[count.index]}"  
-  } 
 }
